14bbe7a58dd199adce574dcc2d8e1cf4f8e2b1fa
nakhun9279 <fjfj9279@naver.com>
2020-09-04 22:35:27 +0900

    22.7 데이터 조회와 22.8 데이터 삭제와 수정

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -4,9 +4,9 @@ import Post from '../../models/post';
 /*
   POST /api/posts
   {
-    title: ‘제목’,
-    body: ‘내용’,
-    tags: [‘태그1’, ‘태그2’]
+    title: '제목',
+    body: '내용',
+    tags: ['태그1', '태그2']
   }
 */
 export const write = async ctx => {
@@ -24,15 +24,69 @@ export const write = async ctx => {
   }
 };
 
+/*
+  GET /api/posts
+*/
+export const list = async ctx => {
+  try {
+    const posts = await Post.find().exec();
+    ctx.body = posts;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+/*
+  GET /api/posts/:id
+*/
+export const read = async ctx => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findById(id).exec();
+    if (!post) {
+      ctx.status = 404; // Not Found
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
 
-
-export const read =  ctx => {};
-
-
-
-export const remove = ctx => {};
-
-
-
-export const update = ctx => {};
+/*
+  DELETE /api/posts/:id
+*/
+export const remove = async ctx => {
+  const { id } = ctx.params;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터는 없음)
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+/*
+  PATCH /api/posts/:id
+  {
+    title: ‘수정’,
+    body: ‘수정 내용’,
+    tags: [‘수정’, ‘태그’]
+  }
+*/
+export const update = async ctx => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
+      // false일 때는 업데이트되기 전의 데이터를 반환합니다.
+    }).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
