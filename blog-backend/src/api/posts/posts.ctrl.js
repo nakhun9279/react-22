bb3a07e645cb9bdd5372ab3d60abe80caaab753a
nakhun9279 <fjfj9279@naver.com>
2020-09-04 22:56:50 +0900

    22.10.3 보이는 개수 제한

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -59,17 +59,18 @@ export const write = async ctx => {
   GET /api/posts
 */
 
-  export const list = async ctx => {
-    try {
-      const posts = await Post.find()
-        .sort({ _id: -1 })
-        .exec();
-      ctx.body = posts;
-    } catch (e) {
-      ctx.throw(500, e);
-    }
-  };
 
+    export const list = async ctx => {
+      try {
+        const posts = await Post.find()
+          .sort({ _id: -1 })
+          .limit(10)
+          .exec();
+        ctx.body = posts;
+      } catch (e) {
+        ctx.throw(500, e);
+      }
+    };
 /*
   GET /api/posts/:id
 */
