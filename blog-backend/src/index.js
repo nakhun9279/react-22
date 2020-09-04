1265149bb2ef0d84554eef2b526695b9a56d11b0
nakhun9279 <fjfj9279@naver.com>
2020-09-04 17:31:35 +0900

    21.5.5.2 컨트롤러 파일작성

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 
 const api = require('./api');
@@ -16,6 +17,11 @@ router.use('/api', api.routes()); // api 라우트 적용
 
 
 
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
+
+
+
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
@@ -24,4 +30,3 @@ app.use(router.routes()).use(router.allowedMethods());
 app.listen(4000, () => {
   console.log('Listening to port 4000');
 });
-
